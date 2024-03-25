f563817d452d6f8d344e17d8175a0fdec2d2381e
Monica Palko <m.a.palko07@gmail.com>
2024-03-24 23:49:57 -0400

    updated the character counter to use the this keyword

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,14 @@
-$(document).ready(function() {
-  // Character counter function
-  function updateCounter() {
-    const maxLength = 140;
-    const currentLength = $('#tweet-text').val().length;
-    const charsLeft = maxLength - currentLength;
-    const counter = $('.counter'); // Select by class instead of id
+$('#tweet-text').on('input', function() {
+  const maxLength = 140;
+  const currentLength = $(this).val().length;
+  const charsLeft = maxLength - currentLength;
+  const counter = $(this).siblings('div').find('.counter');
 
-    counter.text(charsLeft);
+  counter.text(charsLeft);
 
-    // Change counter color based on remaining characters
-    if (charsLeft < 0) {
-      counter.css('color', 'red');
-    } else {
-      counter.css('color', 'black');
-    }
+  if (charsLeft < 0) {
+    counter.css('color', 'red');
+  } else {
+    counter.css('color', 'black');
   }
-
-  // Update counter on input
-  $('#tweet-text').on('input', updateCounter);
 });
